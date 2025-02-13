7482fb9a7ff242ed2f8eeaea888ed8004757957d
unknown <alphonso.saiewane@gmail.com>
2025-02-13 13:57:10 -0500

    Compute the mean and standard deviation

--- homework1_solution.R
+++ homework1_solution.R
@@ -4,7 +4,9 @@ random_numbers <- rnorm(100, mean = 25, sd = 1)
 
 # Compute the mean and standard deviation
 mean_value <- mean(random_numbers)
+mean_value
 sd_value <- sd(random_numbers)
+sd_value
 
 # Print the results
 print(paste("The Mean is", mean_value, "and the Standard Deviation is", sd_value))
